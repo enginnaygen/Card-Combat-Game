95f57177e34eb51f8ded230be991b7d06dc1affd
Engin <enginaygen77@gmail.com>
2024-11-22 13:44:55 +0300

    Created Card SO for storing data

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,22 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] int health;
+    [SerializeField] CardScriptableObject cardSO;
+
+    [SerializeField] int currentHealth;
     [SerializeField] int attackPower;
     [SerializeField] int manaCost;
+
     [SerializeField] TMP_Text healthText;
     [SerializeField] TMP_Text attackPowerText;
     [SerializeField] TMP_Text costText;
+    [SerializeField] TMP_Text nameText;
+    [SerializeField] TMP_Text actionDescriptionText;
+    [SerializeField] TMP_Text loreText;
+
+    [SerializeField] Image characterArt;
+    [SerializeField] Image backgroundArt;
 
 
     void Start()
     {
-        healthText.text = health.ToString();
+        SetupCard();
+    }
+
+    private void SetupCard()
+    {
+        currentHealth = cardSO.currentHealth;
+        attackPower = cardSO.attackPower;
+        manaCost = cardSO.manaCost;
+
+        nameText.text = cardSO.cardName;
+        actionDescriptionText.text = cardSO.actionDescription;
+        loreText.text = cardSO.cardLore;
+
+        healthText.text = currentHealth.ToString();
         attackPowerText.text = attackPower.ToString();
         costText.text = manaCost.ToString();
+
+        characterArt.sprite = cardSO.characterSprite;
+        backgroundArt.sprite = cardSO.bgSprite;
     }
 
     // Update is called once per frame
